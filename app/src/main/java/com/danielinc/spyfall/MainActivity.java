f74d0092236f264f0b2cba544c242c9053062299
דניאל פרידקין <57833477+danielfree19@users.noreply.github.com>
2021-09-30 20:58:40 +0300

    daniel update update of Rules for the game

--- app/src/main/java/com/danielinc/spyfall/MainActivity.java
+++ app/src/main/java/com/danielinc/spyfall/MainActivity.java
@@ -2,7 +2,6 @@ package com.danielinc.spyfall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         config = findViewById(R.id.config);
         Main = findViewById(R.id.mainWindow);
-        Rules=findViewById(R.id.RulesBtn);
-
+        Rules = findViewById(R.id.Rules);
         Main.setOnClickListener(v->{
             setMainWin(v);
         });
@@ -25,18 +23,26 @@ public class MainActivity extends AppCompatActivity {
             setConfig(v);
         });
         Rules.setOnClickListener(v->{
-
+            setRules(v);
         });
         Main.callOnClick();
     }
     public void setMainWin(View v){
         Main.setVisibility(v.GONE);
         config.setVisibility(v.VISIBLE);
+        Rules.setVisibility(v.VISIBLE);
         getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new MainLogin()).commit();
     }
     public void setConfig(View v){
         Main.setVisibility(v.VISIBLE);
         config.setVisibility(v.GONE);
+        Rules.setVisibility(v.VISIBLE);
         getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new Config()).commit();
+    }
+    public void setRules(View v){
+        Main.setVisibility(v.VISIBLE);
+        config.setVisibility(v.VISIBLE);
+        Rules.setVisibility(v.GONE);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new game_rules_fragment()).commit();
     }
 }
